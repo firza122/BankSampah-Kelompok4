8a546b5ed02e55af4585ac6d3dc25928ab4c1df5
firza122 <firzaajikarya112@gmail.com>
2023-10-24 11:36:01 +0700

    Initial commit

---
+++ app/src/main/java/com/example/banksampahkelompok4/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.banksampahkelompok4
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+    }
+}
