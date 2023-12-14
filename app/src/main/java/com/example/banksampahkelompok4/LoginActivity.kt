a48bb5893e67425153f3ea579027a7f596f61dd4
firza122 <firzaajikarya112@gmail.com>
2023-12-14 11:42:45 +0700

    login,regrister,menyambungkan ke firebase

--- app/src/main/java/com/example/banksampahkelompok4/LoginActivity.kt
+++ app/src/main/java/com/example/banksampahkelompok4/LoginActivity.kt
@@ -1,11 +1,65 @@
 package com.example.banksampahkelompok4
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var editEmail:EditText
+    private lateinit var editUsername:EditText
+    private lateinit var editPassword:EditText
+    private lateinit var btnRegister: ImageView
+    private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var firestore: FirebaseFirestore
+    private lateinit var editLogin: TextView
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        editEmail=findViewById(R.id.email)
+        editUsername=findViewById(R.id.username)
+        editPassword=findViewById(R.id.password)
+        btnRegister = findViewById(R.id.signup)
+        firebaseAuth = FirebaseAuth.getInstance()
+        firestore = FirebaseFirestore.getInstance()
+        editLogin = findViewById(R.id.signinlogin)
+
+        btnRegister.setOnClickListener {
+            val email = editEmail.text.toString()
+            val username = editUsername.text.toString()
+            val password = editPassword.text.toString()
+
+            if (email.isEmpty() || username.isEmpty() || password.isEmpty()){
+                return@setOnClickListener
+            }else{
+                prosesRegister(email,password,username)
+            }
+        }
+        editLogin.setOnClickListener {
+            val toLogin = Intent(this,RegisterActivity::class.java)
+            startActivity(toLogin)
+        }
+    }
+    fun prosesRegister(email:String,password: String,username: String){
+        firebaseAuth.createUserWithEmailAndPassword(email,password).addOnSuccessListener {
+            val user = hashMapOf(
+                "email" to email,
+                "username" to username
+            )
+            firestore.collection("user").add(user).addOnSuccessListener {
+                val intent = Intent(this, RegisterActivity::class.java)
+                startActivity(intent)
+            }
+        }.addOnFailureListener {
+            Toast.makeText(this, "Gagal Login", Toast.LENGTH_SHORT).show()
+        }
     }
 }
--- app/src/main/java/com/example/banksampahkelompok4/RegisterActivity.kt
+++ app/src/main/java/com/example/banksampahkelompok4/RegisterActivity.kt
@@ -1,11 +1,47 @@
 package com.example.banksampahkelompok4
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 
 class RegisterActivity : AppCompatActivity() {
+    private lateinit var editemaillogin : EditText
+    private lateinit var passwordlogin : EditText
+    private lateinit var buttonlogin :   ImageView
+    private lateinit var signin : TextView
+    private lateinit var firebaseAuth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+        editemaillogin = findViewById(R.id.emaillogin)
+        passwordlogin = findViewById(R.id.passwordlogin)
+        buttonlogin = findViewById(R.id.loginbtn)
+        signin = findViewById(R.id.signinlogin)
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        buttonlogin.setOnClickListener {
+            val email = editemaillogin.text.toString()
+            val password = passwordlogin.text.toString()
+
+            if (email.isEmpty() || password.isEmpty()){
+                return@setOnClickListener
+            }else{
+                prosesLogin(email,password)
+            }
+        }
+
+    }
+    fun prosesLogin (email: String,password: String){
+        firebaseAuth.signInWithEmailAndPassword(email,password).addOnSuccessListener {
+            val toMain = Intent(this,MainActivity::class.java)
+            startActivity(toMain)
+        }.addOnFailureListener {
+            Toast.makeText(this, it.toString(), Toast.LENGTH_SHORT).show()
+        }
     }
 }
